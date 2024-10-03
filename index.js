0755b8c800038bbcc526a321accf56ff7cabd44c
drumovski <jason.stacy@gmail.com>
2024-10-03 17:11:22 +1000

    added chat message fnction

--- index.js
+++ index.js
@@ -11,16 +11,25 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+    //add socket.on functions here:
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+
 });
 
+/* moved this duplicate function and added it to the io.on function
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
   });
 });
+*/
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
